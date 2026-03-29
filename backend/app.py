ea0822fc0f5aa7864ad416902a3b1754270614e5
charleswereangoye <c.angoye@alustudent.com>
2026-03-29 16:39:34 +0200

    updated my requirement.txt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
+from psycopg.rows import dict_row
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import date, timedelta
